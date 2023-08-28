21c630bf8eb364add21f95fbeae8ab76447d73ed
Kurmi <kurmi.gorle@cerner.com>
2023-08-28 11:50:02 +0530

    Simple change to trigger build

--- src/main/java/com/kurmi/todos/controllers/TodosController.java
+++ src/main/java/com/kurmi/todos/controllers/TodosController.java
@@ -20,6 +20,7 @@ public class TodosController {
         todoItems.add(new Todo("Bike", "Get it cleaned", false));
         todoItems.add(new Todo("Car", "You don't have car", false));
         todoItems.add(new Todo("Go home", "No need to remind", false));
+        todoItems.add(new Todo("Office", "Do your work", false));
     }
 
     @GetMapping("/")
